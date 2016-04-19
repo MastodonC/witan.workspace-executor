d517e208da666e2b7b4b9303d1060ade5763f6b4
Antony Woods <antony@mastodonc.com>
2016-04-19 16:20:35 +0100

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject witan.workspace-executor "0.1.3"
+(defproject witan.workspace-executor "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
